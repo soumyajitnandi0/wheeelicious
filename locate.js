02759a7b9d8c9fa3a46f2efa694003c10217f2ad
soumyajitnandi0 <soumyajitnandi7384@gmail.com>
2024-12-12 15:11:54 +0530

    directory decorated

--- locate.js
+++
@@ -1,5 +0,0 @@
-// Leaflet Map
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
